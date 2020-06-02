09fff7a2dc4b9d1f5f12c0d6acbb3f303f24a836
GoncaloBatista <goncalofbatista@hotmail.com>
2020-06-02 16:33:19 +0100

    Updates

---
+++ precache-manifest.7969ceb14a446b3006baa6c0abaa013d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41b714bee6e8d73e16d22f64b2974b6f",
+    "url": "/ip-location/index.html"
+  },
+  {
+    "revision": "28e5ce55dee759349aca",
+    "url": "/ip-location/static/css/main.3d463327.chunk.css"
+  },
+  {
+    "revision": "d822732494d1d2537ded",
+    "url": "/ip-location/static/js/2.a58a9cf2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ip-location/static/js/2.a58a9cf2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28e5ce55dee759349aca",
+    "url": "/ip-location/static/js/main.5de07844.chunk.js"
+  },
+  {
+    "revision": "d9e050554a9b8024e462",
+    "url": "/ip-location/static/js/runtime-main.0fb71e0b.js"
+  }
+]);
